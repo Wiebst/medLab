6832577d875c8403d66619fbd0d304b17bed601c
Nikita <shylushal@MacBookm4.local>
2026-05-12 21:24:18 +0200

    23.04

---
+++ env.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+
+const config = {
+  env: process.env.NODE_ENV || 'development',
+  port: process.env.PORT || 3000,
+  dburl: process.env.DB_URL || '',
+};
+
+module.exports = config;
